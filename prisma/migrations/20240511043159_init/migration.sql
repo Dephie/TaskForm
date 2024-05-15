846902be87180e94c5dd6030b85dadbacb644397
Restyled.io <commits@restyled.io>
2024-05-15 04:38:07 +0000

    Restyled by pg_format

--- prisma/migrations/20240511043159_init/migration.sql
+++ prisma/migrations/20240511043159_init/migration.sql
@@ -1,9 +1,10 @@
 -- CreateTable
 CREATE TABLE "Task" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
-    "description" TEXT,
-    "priority" TEXT NOT NULL DEFAULT 'low',
+    "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" text NOT NULL,
+    "description" text,
+    "priority" text NOT NULL DEFAULT 'low',
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL
 );
+
